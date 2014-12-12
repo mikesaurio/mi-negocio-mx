259fb28d49473b5a831d49b21871bcffe18fcc4c
mikesaurio <mikesaurio@gmail.com>
2014-12-12 16:18:36 -0600

    muestra documentos y tramites necesarios para abrir una empresa

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,23 +3,21 @@ class PagesController < ApplicationController
 
  def quiere
    @titulo = "¿Qué quieres saber?"
-   @subtitulo= "Encuentra toda la información para tu negocio en"
+   @subtitulo= "Encuentra toda la información para tu negocio"
    @param = params[:municipio]
    $id_del_municipio = @param[:municipio_id]
-   @municipio = Municipio.find($id_del_municipio).nombre
  end
 
  def encuesta
-    @titulo = "Tramites"
-   @subtitulo= "Primero contesta esta breve encuesta"
+  @titulo = 'Encuesta de entrada'
  end
 
  def paso
- @titulo = "Tramites"
   @param_paso = params[:lines]
   $id_del_giro = @param_paso[:line_id]
-  @subtitulo = Line.find($id_del_giro).nombre
-  @subtitulo= "Abrir y operar #{@subtitulo} "
+  @subtitulo = "Para tu negocio  de  #{Line.find($id_del_giro).nombre} necesitas"
+  @tramites_del_giro = Line.find($id_del_giro).procedures.includes(:procedure_lines).where("line_id = #{$id_del_giro}")
+
 
 end
 
@@ -34,4 +32,11 @@ end
 def tramite
  @titulo = Municipio.find($id_del_municipio).nombre    
 end
+
+def tramites_giro(num)
+  Procedure.find(num).requirements.includes(:procedure_requirements).where("procedure_id = #{num}")
 end
+helper_method :tramites_giro
+
+end
+
