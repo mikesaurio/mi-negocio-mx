7a35398a2b20c8724ffc971b0a313e2c483fdd21
mikesaurio <mikesaurio@gmail.com>
2014-12-11 19:47:59 -0600

    se muestran nombre de los tramites que se deben realizar por giro

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,20 +3,23 @@ class PagesController < ApplicationController
 
  def quiere
    @titulo = "¿Qué quieres saber?"
-   @subtitulo= "Encuentra toda la información para tu negocio"
+   @subtitulo= "Encuentra toda la información para tu negocio en"
    @param = params[:municipio]
    $id_del_municipio = @param[:municipio_id]
+   @municipio = Municipio.find($id_del_municipio).nombre
  end
 
  def encuesta
-  @titulo = 'Encuesta de entrada'
+    @titulo = "Tramites"
+   @subtitulo= "Primero contesta esta breve encuesta"
  end
 
  def paso
-  @titulo= "Para tu negocio necesitas"
+ @titulo = "Tramites"
   @param_paso = params[:lines]
   $id_del_giro = @param_paso[:line_id]
   @subtitulo = Line.find($id_del_giro).nombre
+  @subtitulo= "Abrir y operar #{@subtitulo} "
 
 end
 
