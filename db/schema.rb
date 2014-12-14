b8e3ac988104f36e19b699887fdc4c24a35db73b
Raúl Jiménez <jimenezjrs@gmail.com>
2014-12-14 14:31:02 -0800

    create a migration to change length of fields for three properties in the inspection model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209182034) do
+ActiveRecord::Schema.define(version: 20141214222351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,9 +44,9 @@ ActiveRecord::Schema.define(version: 20141209182034) do
     t.string   "materia"
     t.string   "duracion"
     t.string   "norma"
-    t.string   "antes"
-    t.string   "durante"
-    t.string   "despues"
+    t.text     "antes"
+    t.text     "durante"
+    t.text     "despues"
     t.string   "sancion"
     t.datetime "created_at"
     t.datetime "updated_at"
