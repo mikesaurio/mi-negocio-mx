4f8fdf1987734268fd4ad19d05f8dffe00b55648
mikesaurio <mikesaurio@gmail.com>
2014-12-19 15:25:02 -0600

    se agrega filtro persona moral y fisica

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,11 +18,13 @@ def tramite
   @procedure = Procedure.all
   #@tramites_del_giro = Line.all
 @id_del_giro = "0"
+@tipo  = 'A'
 unless params[:lines].nil?
   @param_paso = params[:lines]
   puts @param_paso.methods
   @id_del_giro = @param_paso[:line_id]
   @tramites_del_giro = Line.find(@id_del_giro).procedures.includes(:procedure_lines).where("line_id = #{@id_del_giro}") 
+  @tipo = params[:rating]
 end
 
 
