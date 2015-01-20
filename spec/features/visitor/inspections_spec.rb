7961c723081f903de2cb3c3676d00384f36b8389
Raúl Jiménez <jimenezjrs@gmail.com>
2015-01-20 17:07:13 -0600

    implemented first tests for the inspection module

--- spec/features/visitor/inspections_spec.rb
+++ spec/features/visitor/inspections_spec.rb
@@ -1,15 +1,60 @@
 require 'rails_helper'
 
 feature 'A visitor enters the page' do
+  attr_reader :city, :inspections
 
   before(:all) do
+    @city = create :municipio
+    dependency = create :dependency, municipio: city
     @inspections = []
-    3.times { @inspections << create :inspection }
+    3.times { @inspections << create(:inspection, dependency: dependency) }
   end
 
-  scenario 'and sees the root page' do
+  scenario 'and goes to the city inspections list' do
+    # Given a city's, go to the inspections page
+    visitor_navigates_to_city_inspections_from(city)
+
+    # Are we in the correct path?
+    expect(current_path).to eq municipio_inspections_path(city)
+
+    # Check that there is a listing with all 3 inspections created before
+    inspections.each do |inspection|
+      expect(page).to have_content inspection.nombre
+    end
+  end
+
+  scenario 'a visits the last inspection from the list' do
+    visitor_navigates_to_city_inspections_from(city)
+
+    # click on the last inspection from the list
+    visit_last_inspection_detail
+
+    # let's check that the path is equal to the the last isnpection :show path
+    # for this resource
+    raise current_path.inspect
+    expect(current_path).to eq(municipio_inspection_path(last_inspection))
+
+    # let's check for 'some' information of the resource
+    expect(page).to have_content last_inspection.nombre
+    expect(page).to have_content last_inspection.materia
+    expect(page).to have_content last_inspection.duracion
+
+    # save_and_open_page
+  end
+
+
+  def visitor_navigates_to_city_inspections_from(current_city)
     visit root_path
-    save_and_open_page
-    expect(page).to have_content 'Entrar'
+    select current_city.nombre, from: 'post_municipio'
+    click_on 'Continuar'
+    click_on 'Inspecciones'
+  end
+
+  def visit_last_inspection_detail
+    click_on last_inspection.nombre
+  end
+
+  def last_inspection
+    inspections.last
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,6 +57,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  config.include FactoryGirl::Syntax::Methods
 
   # Database cleaner setup
   config.before(:suite) do
