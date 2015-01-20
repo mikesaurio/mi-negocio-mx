7bd09a2afbf09071dd8156444cab4ea1274d7d03
mikesaurio <mikesaurio@gmail.com>
2015-01-20 16:41:09 -0600

    se agregan traducciones formation steps

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -9,13 +9,9 @@ class InspectionsController < ApplicationController
     else
       @inspections = Inspection.all
     end
-
-    cargar_menus
   end
 
   def show
-
-    cargar_menus
   end
 
   private
@@ -32,13 +28,6 @@ class InspectionsController < ApplicationController
     end
 
 
-     def cargar_menus
-      @link_1 ="/dashboard"
-     @texto_link_1 = "Apertura"
-      @link_2="/dashboard"
-     @texto_link_2 = "Tramites"
-      @link_3 =""
-     @texto_link_3 = ""
-    end
+  
 
 end
