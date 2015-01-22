c57908f3b9b428ff94d8f75a65702dd258cf615f
mikesaurio <mikesaurio@gmail.com>
2015-01-21 18:41:23 -0600

    se agrega helper a formation_step

---
+++ app/helpers/formation_steps_helper.rb
@@ -0,0 +1,11 @@
+module FormationStepsHelper
+
+  def heading_search(name)
+    "Apertura de #{name}"
+  end
+
+  def subheading_search(type)
+    "Para persona #{type == 'AF'? 'física' : 'moral'}"
+  end
+
+end
