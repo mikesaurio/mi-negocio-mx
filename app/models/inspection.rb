8396abe19ad434f220614881555df4e2fff68584
mikesaurio <mikesaurio@gmail.com>
2015-02-09 18:51:04 -0600

    csv y xml para inspections

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -23,26 +23,28 @@ def self.to_csv(options = {})
     CSV.generate(options) do |csv|
       csv << ["nombre","materia","duracion", "norma","antes","durante","despues","sancion","dependencia","giros","requerimientos"]#column_names
       all.each do |product|
-        csv << [product.nombre,product.materia,product.norma,product.antes,product.durante,product.despues,product.sancion,
-          Dependency.find(product.dependency_id).nombre,procedure_lines(product.id),procedure_requirements(product.id)] 
+        csv << [
+          product.nombre,
+          product.materia,
+          product.duracion,
+          product.norma,
+          product.antes,
+          product.durante,
+          product.despues,
+          product.sancion,
+          Dependency.find(product.dependency_id).nombre,
+          inspection_lines(product.id),
+          inspection_requirements(product.id)] 
         end
       end
     end
   end
 
-  def tipo_tramite(tramite)
-   if tramite == 'TF'
-    'Física'
-  elsif  tramite == 'TM'
-    'Moral'
-  else
-    'Ambas'
-  end
 
 
-  def procedure_lines(id_procedure)
+  def inspection_lines(id_inspection)
     aux = []
-    ProcedureLine.where(procedure_id: id_procedure).each_with_index do |line, index|
+    InspectionLine.where(inspection_id: id_inspection).each_with_index do |line, index|
       aux[index] = Line.find(line.line_id).nombre
     end
     if aux.blank?
@@ -53,9 +55,9 @@ def self.to_csv(options = {})
   end
 
 
-def procedure_requirements(id_procedure)
+def inspection_requirements(id_inspection)
   aux = []
-  ProcedureRequirement.where(procedure_id: id_procedure).each_with_index do |requirement, index|
+  InspectionRequirement.where(inspection_id: id_inspection).each_with_index do |requirement, index|
     aux[index] = Requirement.find(requirement.requirement_id).nombre
   end
   if aux.blank?
@@ -65,4 +67,4 @@ def procedure_requirements(id_procedure)
   end
 end
 
-end
+
